1f5023b4630340b73aac1d731c892a633063cda3
beregd1 <119509393+beregd1@users.noreply.github.com>
2022-12-20 21:31:41 +0200

    first homework3

--- src/it/sourse/homework1/FindNumbers.java
+++ src/it/sourse/homework1/FindNumbers.java
@@ -11,9 +11,25 @@ public class FindNumbers {
         System.out.println(findNumbers(src2));
     }
 
-    static S findNumbers(String text) {
-        int result = 0;
+    static String findNumbers(String text) {
+        StringBuilder result = new StringBuilder();
+        boolean numberWasFound = false;
+        for(int i = 0; i<text.length(); i++){
+            char ch = text.charAt(i);
+            if(Character.isDigit(ch)) {
+                result.append(ch);
+                numberWasFound = true;
+            }else {
+                if(numberWasFound){
+                    result.append(",");
+                }
+                numberWasFound = false;
+            }
 
-
+        }
+        if(result.toString().endsWith(",")){
+            result.deleteCharAt(result.toString().lastIndexOf(","));
+        }
+        return result.toString();
     }
 }
